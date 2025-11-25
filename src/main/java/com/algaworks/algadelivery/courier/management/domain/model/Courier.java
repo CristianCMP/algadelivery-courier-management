a05005f6d47b4318633e5876fbefaa9b707aabc3
Cristian Puhl <cristian.puhl@universo.univates.br>
2025-11-25 15:53:34 -0300

    Implementing Endpoints for Courier Registration

--- src/main/java/com/algaworks/algadelivery/courier/management/domain/model/Courier.java
+++ src/main/java/com/algaworks/algadelivery/courier/management/domain/model/Courier.java
@@ -1,9 +1,6 @@
 package com.algaworks.algadelivery.courier.management.domain.model;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.OffsetDateTime;
@@ -35,7 +32,7 @@ public class Courier {
 
     private OffsetDateTime lastFulfilledDeliveryAt;
 
-    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "courier")
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "courier")
     private List<AssignedDelivery> pendingDeliveries = new ArrayList<>();
 
     public List<AssignedDelivery> getPendingDeliveries() {
