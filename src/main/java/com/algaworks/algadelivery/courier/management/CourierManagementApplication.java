89af9b48bbf2c9153d097a694db42c18f15f9a0d
Cristian Puhl <cristian.puhl@universo.univates.br>
2025-11-27 17:12:02 -0300

    Integrating microservices into Eureka

--- src/main/java/com/algaworks/algadelivery/courier/management/CourierManagementApplication.java
+++ src/main/java/com/algaworks/algadelivery/courier/management/CourierManagementApplication.java
@@ -2,8 +2,10 @@ package com.algaworks.algadelivery.courier.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CourierManagementApplication {
 
