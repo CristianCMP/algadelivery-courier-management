9e8c61a1e5b328972c481dfe1765dcaaa42a399f
Cristian Puhl <cristian.puhl@universo.univates.br>
2025-11-27 16:13:32 -0300

    Business logic for assignment

--- src/main/java/com/algaworks/algadelivery/courier/management/CourierManagementApplication.java
+++ src/main/java/com/algaworks/algadelivery/courier/management/CourierManagementApplication.java
@@ -2,6 +2,7 @@ package com.algaworks.algadelivery.courier.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 public class CourierManagementApplication {
